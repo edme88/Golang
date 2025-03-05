166533b42f2f916f0bc3788cc0424ad6202f341d
agustina-aliciardi <agustina.aliciardi@darwoft.com>
2025-03-04 23:01:31 -0300

    add 97

--- 13-AppWeb/wiki.go
+++ 13-AppWeb/wiki.go
@@ -26,8 +26,10 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hola! %s", r.URL.Path[1:])
+func viewHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1> <div>%s</div>", p.Title, p.Body)
 }
 
 func main(){
@@ -37,6 +39,6 @@ func main(){
 	// p2, _ := loadPage("TestPage")
 	// fmt.Println(string(p2.Body))
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil)) //detiene el servidor si hay error
 }
