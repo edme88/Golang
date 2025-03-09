30a0c3919526cfad6f4fc7f1f8e20b6b8febe2cf
agustina-aliciardi <agustina.aliciardi@darwoft.com>
2025-03-08 21:10:30 -0300

    add 129 uso de muts

--- 15-web-introduccion/handlers/handlers.go
+++ 15-web-introduccion/handlers/handlers.go
@@ -3,41 +3,49 @@ package handlers
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+)
+
+const (
+	templateDir = "templates/"
+	templateBase = templateDir + "base.html"
 )
 
 //recibe una url y una función handler
 //para registrar un controlador
-func Index(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintln(w, "<h1>Página de Inicio!</h1>") //No usar html
-	//tpl, err := template.ParseFiles("templates/index.html") //Analiza una lista del nombre de las plantillas
-	tpl, err := template.ParseFiles("templates/base.html","templates/index.html")
-	if err != nil {
-		http.Error(w, "Error al analizar plantillas", http.StatusInternalServerError)
-	}
+//func Index(w http.ResponseWriter, r *http.Request) {
+//fmt.Fprintln(w, "<h1>Página de Inicio!</h1>") //No usar html
+//tpl, err := template.ParseFiles("templates/index.html") //Analiza una lista del nombre de las plantillas
+// tpl, err := template.ParseFiles("templates/base.html","templates/index.html")
+// if err != nil {
+// 	http.Error(w, "Error al analizar plantillas", http.StatusInternalServerError)
+// }
 
-	// //Para enviar datos se pueden emplear estructuras o mapas
-	// //de manera directa crear estrictura e inicializar los datos
-	// data := struct {
-	// 	Title string
-	// 	Message string
-	// }{
-	// 	Title: "Página de inicio",
-	// 	Message: "Bienvenido a Pidra, Papel o Tijera",
-	// }
-	// err = tpl.Execute(w, data)
-	// if err != nil{
-	// 	http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
-	// }
-	err = tpl.ExecuteTemplate(w, "base", nil)
-	if err != nil{
-		http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
-	}
+// //Para enviar datos se pueden emplear estructuras o mapas
+// //de manera directa crear estrictura e inicializar los datos
+// data := struct {
+// 	Title string
+// 	Message string
+// }{
+// 	Title: "Página de inicio",
+// 	Message: "Bienvenido a Pidra, Papel o Tijera",
+// }
+// err = tpl.Execute(w, data)
+// if err != nil{
+// 	http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
+// }
+// 	err = tpl.ExecuteTemplate(w, "base", nil)
+// 	if err != nil{
+// 		http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
+// 	}
+// }
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html", nil)
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
-
-	
 	fmt.Fprintln(w, "Crear nuevo juego")
 }
 
@@ -51,4 +59,16 @@ func Play(w http.ResponseWriter, r *http.Request) {
 
 func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Acerca de")
+}
+
+func renderTemplate(w http.ResponseWriter, page string, data any){
+	tpl := template.Must(template.ParseFiles(templateBase, templateDir+page))
+
+	err := tpl.ExecuteTemplate(w, "base", data)
+	if err != nil{
+		http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
 }
